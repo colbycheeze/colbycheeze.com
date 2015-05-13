3a58e66e2d45f406745e3e36f6bb4974cf60f4d5
Colby "CheeZe" Williams <colby@colbycheeze.com>
2015-05-13 07:30:30 -0500

    Added responsive slide-out nav that works on mobile also

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -29,3 +29,9 @@ $(function() {
   }).resize();
 });
 
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
